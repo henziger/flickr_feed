0497daa6115a1cbad75680742f96fc876878e2ec
Eric Henziger <hemtillhenziger@gmail.com>
2018-02-17 12:14:45 +0100

    Rework image list styling.

--- src/ImageListItem.js
+++ src/ImageListItem.js
@@ -6,16 +6,16 @@ class ImageListItem extends Component {
 
   render () {
     return (
-      <div className="container imageItem">
-        <div className="imagePreview" onClick={this.props.setImage}>
+      <div className="container image-item">
+        <div className="image-preview" onClick={this.props.setImage}>
           <img src={this.props.media} alt={this.props.title}/>
         </div>
-        <div className="imageSummary">
-          <div onClick={this.props.setImage}><h2 className="imageTitle">{this.props.title}</h2></div>
-          <div className="container">
-            <p>By <a href={this.props.authorUrl}>{getAuthorName(this.props.author)}</a></p>
-            <p>Published: {this.props.published}</p>
-            <p><a href={this.props.link}>View on Flickr</a></p>
+        <div className="image-summary">
+          <div onClick={this.props.setImage}><h2 className="image-title">{this.props.title}</h2></div>
+          <div className="container image-meta">
+            <div className="image-author">By <a href={this.props.authorUrl}>{getAuthorName(this.props.author)}</a></div>
+            <div>Published: {this.props.published}</div>
+            <div><a href={this.props.link}>View on Flickr</a></div>
           </div>
         </div>
       </div>
