b5f1f7345ed4b9c3a84fa45157a441e051c95bee
Eric Henziger <hemtillhenziger@gmail.com>
2018-02-25 13:12:45 +0100

    Attempt some cleanup of CSS and add responsivity.

--- src/Header.js
+++ src/Header.js
@@ -10,17 +10,15 @@ class Header extends Component {
   render() {
     return (
       <div className="App-header">
-        <h1 className="App-title align-header">Flickr Public Feed</h1>
+        <div className="App-title align-header">Flickr Public Feed</div>
         <div className="search-container">
-          <form onSubmit={this.props.makeQuery}>
-            <input className="search-field" type="text" value={this.props.searchField} onChange={this.props.updateField}
-                   placeholder={"Search"}
-                   ref={(input) => { this.searchBar = input; }}
-            />
-            <button type="submit">
-              <i className="fa fa-search"/>
-            </button>
-          </form>
+          <input className="search-field" type="text" value={this.props.searchField} onChange={this.props.updateField}
+                 placeholder={"Search"}
+                 ref={(input) => { this.searchBar = input; }}
+          />
+          <div className="search-button" onClick={this.props.makeQuery}>
+            <i className="fa fa-search"/>
+          </div>
         </div>
       </div>
     )
